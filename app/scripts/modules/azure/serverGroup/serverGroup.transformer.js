636d3fe21f89bf91b101def7df501259e9707492
JargoonPard <larry.guger@hotmail.com>
2016-01-27 14:46:09 -0800

    Corrected lint warnings

--- app/scripts/modules/azure/serverGroup/serverGroup.transformer.js
+++ app/scripts/modules/azure/serverGroup/serverGroup.transformer.js
@@ -33,36 +33,36 @@ module.exports = angular.module('spinnaker.azure.serverGroup.transformer', [
         detail: base.details,
         credentials: base.credentials,
         region: base.region,
-        user: "[anonymous]",
-        upgradePolicy: "Manual",
+        user: '[anonymous]',
+        upgradePolicy: 'Manual',
 
         image: {
-          publisher: "Canonical",
-          offer: "UbuntuServer",
-          sku: "15.04",
-          version: "latest",
+          publisher: 'Canonical',
+          offer: 'UbuntuServer',
+          sku: '15.04',
+          version: 'latest',
         },
 
         sku: {
-          name: "Standard_A1",
-          tier: "Standard",
+          name: 'Standard_A1',
+          tier: 'Standard',
           capacity: 2,
         },
 
         osConfig: {
-          adminUsername: "spinnakeruser",
-          adminPassword: "!Qnti**234",
+          adminUsername: 'spinnakeruser',
+          adminPassword: '!Qnti**234',
         },
 
         stageDetals: {
-          "type": "createServerGroup",
+          'type': 'createServerGroup',
         },
       };
-      
-      if (typeof base.stack != 'undefined'){
+
+      if (typeof base.stack != 'undefined') {
         command.name = command.name + '-' + base.stack;
       }
-      if(typeof base.details != 'undefined'){
+      if(typeof base.details != 'undefined') {
         command.name = command.name + '-' + base.details;
       }
 
