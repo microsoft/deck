0251d91b55a20a84f825dacfef883185dc6a6372
JargoonPard <larry.guger@hotmail.com>
2016-03-08 08:35:28 -0800

    A bit more cleanup

--- app/scripts/modules/azure/serverGroup/configure/wizard/basicSettings/ServerGroupBasicSettings.controller.js
+++ app/scripts/modules/azure/serverGroup/configure/wizard/basicSettings/ServerGroupBasicSettings.controller.js
@@ -36,11 +36,4 @@ module.exports = angular.module('spinnaker.azure.serverGroup.configure.basicSett
       $uibModalStack: $uibModalStack,
       $state: $state,
     }));
-  }).filter('regional', function(_) {
-  return function(input, selectedRegion) {
-    var result = _.filter(input, function(image) {
-      return image.region === selectedRegion || image.region == null;
-    });
-    return result;
-  };
-});
+  });
